4de55fec2cb89f33d2f704a62994756d2d3956e5
B0elena <e.mc2.1012md@gmail.com>
2021-02-16 13:05:41 +0900

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 須賀辰則 on 2021/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
